2671fe9bb1668aeaf309f9c1d893a1cb0dbbe8d6
wille ferreira <dev.willeferreira@gmail.com>
2026-05-24 07:35:30 -0400

    auth completo com base no laravel/breeze

--- app/Http/Controllers/Api/V1/Auth/RegisterController.php
+++ app/Http/Controllers/Api/V1/Auth/RegisterController.php
@@ -6,7 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\Auth\RegisterRequest;
 use App\Models\User;
 use Database\Factories\UserFactory;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Inertia\Inertia;
 
 class RegisterController extends Controller
@@ -16,7 +19,16 @@ class RegisterController extends Controller
     {
         $data = $request->validated();
 
-        User::create($data);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->string('password')),
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
 
         return redirect()->route('login');
     }
--- routes/Api/V1/auth.php
+++ routes/Api/V1/auth.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Api\V1\Auth\LoginController;
 use App\Http\Controllers\Api\V1\Auth\RegisterController;
+use \App\Http\Controllers\Api\V1\Auth\PasswordResetLinkController;
+use \App\Http\Controllers\Api\V1\Auth\NewPasswordController;
+use App\Http\Controllers\Api\V1\Auth\VerifyEmailController;
+use Inertia\Inertia;
 
 Route::prefix('v1')
     ->name('v1.')
@@ -12,9 +17,29 @@ Route::prefix('v1')
                 Route::post('register', [RegisterController::class, 'register'])
                     ->name('register');
                 Route::post('login', [LoginController::class, 'authenticate'])->name('login');
+                Route::post('password/email', [PasswordResetLinkController::class, 'sendResetLinkEmail'])->name('password.email');
+                Route::post('password/new-password', [NewPasswordController::class, 'store'])->name('password.store');
             });
+
+
     });
 
-Route::get('login', [LoginController::class, 'loginPage'])->name('login');
+Route::get('login', [LoginController::class, 'loginPage'])
+    ->middleware('guest')
+    ->name('login');
 Route::get('register', [RegisterController::class, 'registerPage'])->name('register');
 Route::post('logout', [LoginController::class, 'destroy'])->name('logout');
+Route::get('password-reset', [PasswordResetLinkController::class,'forgotPasswordPage'])->name('password.request');
+Route::get('password-reset/{token}', [NewPasswordController::class,'newPasswordPage'])->name('password.reset');
+
+Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
+    ->middleware(['auth', 'signed', 'throttle:6,1'])
+    ->name('verification.verify');
+
+Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+    ->middleware(['auth', 'throttle:6,1'])
+    ->name('verification.send');
+
+Route::get('/email/verify', function () {
+    return Inertia::render('Auth/VerifyEmail');
+})->name('verification.notice');
